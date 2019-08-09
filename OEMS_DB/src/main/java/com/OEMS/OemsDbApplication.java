6534f2b087cab1ac23e69fc346b55c2faf236a81
jimsweeney <jimsweeney@172.25.150.148>
2019-08-09 15:17:14 -0400

    Initial commit

---
+++ OEMS_DB/src/main/java/com/OEMS/OemsDbApplication.java
@@ -0,0 +1,13 @@
+package com.OEMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OemsDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OemsDbApplication.class, args);
+	}
+
+}
